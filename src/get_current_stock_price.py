352e4ee7eb99b8e4fab4aab8559f5c29da3be9d2
Michael A. Agarenzo <magarenzo@gmail.com>
2021-04-23 11:47:57 -0400

    Implement script.

---
+++ src/get_current_stock_price.py
@@ -0,0 +1,13 @@
+from yahoo_fin import stock_info
+
+# Prompt user to input stock ticker and get current live price
+while True:
+    try:
+        ticker = input("\nWhat is the stock ticker? ")
+        price = str(stock_info.get_live_price(ticker))
+        break
+    except:
+        print("No data found, symbol may be delisted.")
+
+# Print
+print("\nThe current live price of stock " + str.upper(ticker) + " is $" + price + ".")
